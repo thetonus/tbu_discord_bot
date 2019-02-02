04d6ed1e70192137d0df79ac4e39b6be82df806e
Tony Hammack <hammack.tony@gmail.com>
2019-02-02 16:02:18 -0600

    Reformat

--- discord/bot.py
+++ discord/bot.py
@@ -14,7 +14,9 @@ from discord import discord_api
 logger = logging.getLogger(__name__)
 
 
-def article_is_not_db(table: str, article_title: str, article_url: str, article_date: str) -> bool:
+def article_is_not_db(
+    table: str, article_title: str, article_url: str, article_date: str
+) -> bool:
     """ Check if a given pair of article title and date
     is in the database.
     Args:
@@ -26,19 +28,26 @@ def article_is_not_db(table: str, article_title: str, article_url: str, article_
         False if the article is already present in the database
     """
 
-    result = db.table(table).where("title", article_title).where(
-        "link", article_url).where("published", article_date).get().serialize()
+    result = (
+        db.table(table)
+        .where("title", article_title)
+        .where("link", article_url)
+        .where("published", article_date)
+        .get()
+        .serialize()
+    )
 
     if result:
-        logger.debug(
-            f"Article '{article_title}' not found. Needs to be imported.")
+        logger.debug(f"Article '{article_title}' not found. Needs to be imported.")
         return False
 
     logger.debug(f"Article '{article_title}' found. Needs not be imported.")
     return True
 
 
-def add_article_to_db(table: str, article_title: str, article_url: str, article_date: str) -> None:
+def add_article_to_db(
+    table: str, article_title: str, article_url: str, article_date: str
+) -> None:
     """ Add a new article title and date to the database
     Args:
         table (str): current table
@@ -47,11 +56,9 @@ def add_article_to_db(table: str, article_title: str, article_url: str, article_
         article_date (str): The publication date of an article
     """
 
-    db.table(table).insert({
-        'title': article_title,
-        'link': article_url,
-        'published': article_date,
-    })
+    db.table(table).insert(
+        {"title": article_title, "link": article_url, "published": article_date}
+    )
     logger.debug(f"Article '{article_title}' inserted")
 
 
@@ -70,18 +77,20 @@ def read_article_feed(table: str, FEED: str) -> List[str]:
     posts = list()
     raw_feed = feedparser.parse(FEED)
     # Gets the lastest 20 feeds - if not it would download all posts.
-    feed = raw_feed['entries'][0:20]
+    feed = raw_feed["entries"][0:20]
     for article in feed:
-        if article_is_not_db(table, article['title'], article['link'], article['published']):
-            posts.append(
-                {'link': article['link'], 'published': article['published']})
-            add_article_to_db(table,
-                              article['title'],  article['link'], article['published'])
+        if article_is_not_db(
+            table, article["title"], article["link"], article["published"]
+        ):
+            posts.append({"link": article["link"], "published": article["published"]})
+            add_article_to_db(
+                table, article["title"], article["link"], article["published"]
+            )
     return posts
 
 
 def send(hook: str, msg: str) -> None:
-    '''Post message into specific channel on the Discord Server.
+    """Post message into specific channel on the Discord Server.
 
     Arguments:
         webhool {str} -- [Webhook of Discord Server where you want to post the message]
@@ -89,7 +98,7 @@ def send(hook: str, msg: str) -> None:
 
     Returns:
         None
-    '''
+    """
 
     hook = discord_api.Webhook(hook, msg=msg)
     result, status = hook.post()
@@ -101,7 +110,7 @@ def send(hook: str, msg: str) -> None:
 
 
 def run(debug=False) -> None:
-    '''This function starts the Discord Bot. It allows the user to control the runtime enviroment settings.
+    """This function starts the Discord Bot. It allows the user to control the runtime enviroment settings.
 
     Arguments:
         webhooks {Dict} -- [This is a dictionary of all the Discord webhooks for the server.]
@@ -109,43 +118,45 @@ def run(debug=False) -> None:
 
     Returns:
         None
-    '''
+    """
 
     # All the Discord Channels that I care about
     ROOT_DIR = os.getcwd()  # This is your Project Root
-    with open(f'{ROOT_DIR}/configs/channels.json') as f:
+    with open(f"{ROOT_DIR}/configs/channels.json") as f:
         data = f.read()
         channels = json.loads(data)
 
     try:
         for channel in channels["channels"]:
-            logger.info(f'Seeing if {channel} needs to be updated.')
+            logger.info(f"Seeing if {channel} needs to be updated.")
             posts = read_article_feed(
-                DISCORD[channel]["TABLE"], DISCORD[channel]["FEED"])
+                DISCORD[channel]["TABLE"], DISCORD[channel]["FEED"]
+            )
             if len(posts) > 0:
-                logger.info(f'{channel} needs to be updated.')
+                logger.info(f"{channel} needs to be updated.")
 
                 # Sends all new posts from older to newer.
                 def convert(post):
                     """ Convert published date to datetime for comparison """
                     date_str = post["published"][:-6]
                     post["published"] = datetime.datetime.strptime(
-                        date_str, '%a, %d %b %Y %H:%M:%S').date()
+                        date_str, "%a, %d %b %Y %H:%M:%S"
+                    ).date()
                     return post
 
                 # Convert Post's date to datetime
                 posts = [convert(post) for post in posts]
 
                 # Sort posts based on oldest to newest
-                posts = sorted(posts, key=lambda k: k['published'])
+                posts = sorted(posts, key=lambda k: k["published"])
 
                 for post in posts:
                     if not debug:
-                        send(DISCORD[channel]["WEBHOOK"], post['link'])
+                        send(DISCORD[channel]["WEBHOOK"], post["link"])
                     else:
                         logger.info(f"Here is the post: {post['link']}")
             else:
-                logger.info('No new posts')
+                logger.info("No new posts")
 
     except Exception as err:
         logger.exception(err)
